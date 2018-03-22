aa7481fce654c0703e2fba12d965d8512194e800
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-03-22 16:20:24 +0800

    No diagnostics on boost

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <boost/mpi.hpp>
 #pragma GCC diagnostic pop
 

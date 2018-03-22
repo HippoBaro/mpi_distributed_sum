d6052a82e266840dd3d478d6bbc2e1cff757e4a1
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-03-23 01:59:16 +0800

    started refactoring

--- main.cpp
+++ main.cpp
@@ -1,106 +1,98 @@
 #include <iostream>
-#include <sys/time.h>
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+
 #include <boost/mpi.hpp>
 #include <boost/simd.hpp>
 #include <boost/simd/algorithm.hpp>
 #include <boost/simd/memory.hpp>
+
 #pragma GCC diagnostic pop
 
-#define MAX_LEN 4194304
-
-long get_time_us()  // return the time in the unit of us
-{
-    struct timeval my_time;  //us
-    gettimeofday(&my_time, NULL);
-    long runtime_us = 1000000 * my_time.tv_sec + my_time.tv_usec; // us
-    return runtime_us;
+template<typename Function>
+inline auto time_function(Function &&func) {
+    auto begin_time = std::chrono::high_resolution_clock::now();
+    func();
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::high_resolution_clock::now() - begin_time);
 }
 
-int main(int argc, char *argv[])
-{
+template<size_t Size>
+void reduce_and_accumulate(boost::mpi::communicator const &world) {
+    auto local_array = std::vector<int, boost::simd::allocator<int>>(Size);
+    auto reduced_array = std::vector<int, boost::simd::allocator<int>>(Size);
+    auto reduced_array_MPI = std::vector<int, boost::simd::allocator<int>>(Size);
+
+    // the element of array is generated randomly
+    std::generate(local_array.begin(), local_array.end(), [] { return rand(); });
+
+    // MPI_Reduce and then print the usetime, the result will be put in res2[]
+    {
+        world.barrier();
+        auto use_time2 = time_function([&] {
+            boost::mpi::reduce(world, &local_array.front(), static_cast<int>(local_array.size()),
+                               &reduced_array_MPI.front(), std::plus<>(), 0);
+        });
+        if (world.rank() == 0) {
+            std::cout << Size << " int, use_time: " << use_time2.count() << "us [MPI_Reduce]\n";
+        }
+    }
+
+    // YOUR_Reduce and then print the usetime, the result should be put in res[]
+    {
+        world.barrier();
+        auto use_time = time_function([&] {
+            boost::mpi::reduce(world, &local_array.front(), static_cast<int>(local_array.size()),
+                               &reduced_array.front(), std::plus<>(), 0);
+        });
+        if (world.rank() == 0) {
+            std::cout << Size << " int, use_time: " << use_time.count() << "us [YOUR_Reduce]\n";
+        }
+    }
+
+    // check the result of MPI_Reduce and YOUR_Reduce
+    if (world.rank() == 0) {
+        auto correctness = boost::simd::equal(&reduced_array.front(), &reduced_array.back(), &reduced_array_MPI.front());
+        if (!correctness) {
+            printf("WRONG !!!\n");
+        }
+        else {
+            printf("CORRECT !\n");
+        }
+
+        {
+            uint64_t sum = 0;
+            auto use_time = time_function([&] {
+                sum = static_cast<uint64_t>(boost::simd::reduce(&reduced_array.front(), &reduced_array.back(), 0));
+            });
+            std::cout << "sum: " << sum << ", use_time: " << use_time.count() << "us [single thread]\n";
+        }
+
+        {
+            uint64_t sum = 0;
+            auto use_time = time_function([&] {
+                sum = static_cast<uint64_t>(boost::simd::reduce(&reduced_array.front(), &reduced_array.back(), 0));
+            });
+            std::cout << "sum: " << sum << ", use_time: " << use_time.count() << "us [multiple threads]\n";
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
     boost::mpi::environment env{argc, argv};
     boost::mpi::communicator world;
 
-    srand(time(NULL));  // seed for rand() to generate the array randomly
+    srand(1);  // useful for testing
 
-    int count;
-    long begin_time, end_time, use_time, use_time2; // use_time for YOUR_Reduce & use_time2 for MPI_Reduce
-
-    int i;
-
-    // initialize
-    auto a = std::vector<int, boost::simd::allocator<int>>(MAX_LEN); // the array used to do the reduction
-    auto res = std::vector<int, boost::simd::allocator<int>>(MAX_LEN); // the array to record the result of YOUR_Reduce
-    auto res2 = std::vector<int, boost::simd::allocator<int>>(MAX_LEN); // the array to record the result of MPI_Reduce
-    std::fill(a.begin(), a.end(), 0);
-    std::fill(res.begin(), res.end(), 0);
-    std::fill(res2.begin(), res2.end(), 0);
-
-    // TODO
-    // you can add some variable or some other things as you want if needed
-    // TODO
-
-    for(count=4; count<=MAX_LEN; count*=16) // length of array : [ 4  64  1024  16384  262144  4194304 ]
-    {
-        // the element of array is generated randomly
-        for(i=0; i<MAX_LEN; i++)
-        {
-            a[i] = rand() % MAX_LEN;
-        }
-
-        // MPI_Reduce and then print the usetime, the result will be put in res2[]
-        world.barrier();
-        begin_time = get_time_us();
-        boost::mpi::reduce(world, &a.front(), a.size(), &res2.front(), std::plus<>(), 0);
-        world.barrier();
-        end_time = get_time_us();
-        use_time2 = end_time - begin_time;
-        if(world.rank() == 0)
-            printf("%d int use_time : %ld us [MPI_Reduce]\n", count, use_time2);
-
-
-        // YOUR_Reduce and then print the usetime, the result should be put in res[]
-        world.barrier();
-        begin_time = get_time_us();
-        // TODO
-        // you should delete the next line, and do the reduction using your idea
-        boost::mpi::reduce(world, &a.front(), a.size(), &res.front(), std::plus<>(), 0);
-        // TODO
-        world.barrier();
-        end_time = get_time_us();
-        use_time = end_time - begin_time;
-        if(world.rank() == 0)
-            printf("%d int use_time : %ld us [YOUR_Reduce]\n", count, use_time);
-
-
-        // check the result of MPI_Reduce and YOUR_Reduce
-        if(world.rank() == 0)
-        {
-            auto correctness = boost::simd::equal(res.data(), res.data() + res.size(), res2.data());
-            if(!correctness)
-                printf("WRONG !!!\n");
-            else
-                printf("CORRECT !\n");
-
-
-            long sum = 0;
-            begin_time = get_time_us();
-            sum = boost::simd::reduce(res.data(), res.data() + res.size(), 0);
-            end_time = get_time_us();
-            use_time = end_time - begin_time;
-            printf("sum is %ld, use_time : %ld us [single thread]\n", sum, use_time);
-
-            sum = 0;
-            begin_time = get_time_us();
-            sum = boost::simd::reduce(res.data(), res.data() + res.size(), 0);
-            end_time = get_time_us();
-            use_time = end_time - begin_time;
-            printf("sum is %ld, use_time : %ld us [multiple threads]\n\n", sum, use_time);
-        }
-    }
+    reduce_and_accumulate<4>(world);
+    reduce_and_accumulate<64>(world);
+    reduce_and_accumulate<1024>(world);
+    reduce_and_accumulate<16384>(world);
+    reduce_and_accumulate<262144>(world);
+    reduce_and_accumulate<4194304>(world);
     return 0;
 }

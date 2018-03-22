e5da5c4ce58d7ff2ca5aef08608a63292fe1406f
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-03-23 02:54:48 +0800

    Policy-based refactoring

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <numeric>
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
@@ -12,6 +13,38 @@
 
 #pragma GCC diagnostic pop
 
+struct SIMD_accumulator {
+    static constexpr auto name = "SIMD_accumulator";
+    template<typename T>
+    auto operator()(T const* first, T const* last, T init) {
+        return boost::simd::reduce(first, last, init);
+    }
+};
+
+struct STD_accumulator {
+    static constexpr auto name = "STD_accumulator";
+    template<typename T>
+    auto operator()(T const* first, T const* last, T init) {
+        return std::accumulate(first, last, init);
+    }
+};
+
+struct MPI_reduce {
+    static constexpr auto name = "MPI_reduce";
+    template<typename T, typename Op>
+    void operator()(const boost::mpi::communicator &comm, const T *in_values, int n, T *out_values, Op op, int root) {
+        boost::mpi::reduce(comm, in_values, n, out_values, op, root);
+    }
+};
+
+struct YOUR_reduce {
+    static constexpr auto name = "YOUR_reduce";
+    template<typename T, typename Op>
+    void operator()(const boost::mpi::communicator &comm, const T *in_values, int n, T *out_values, Op op, int root) {
+        boost::mpi::reduce(comm, in_values, n, out_values, op, root);
+    }
+};
+
 template<typename Function>
 inline std::chrono::microseconds time_function(Function &&func) {
     auto begin_time = std::chrono::high_resolution_clock::now();
@@ -20,80 +53,42 @@ inline std::chrono::microseconds time_function(Function &&func) {
             std::chrono::high_resolution_clock::now() - begin_time);
 }
 
-template<size_t Size>
-void reduce_and_accumulate(boost::mpi::communicator const &world) {
-    auto local_array = std::vector<int, boost::simd::allocator<int>>(Size);
-    auto reduced_array = std::vector<int, boost::simd::allocator<int>>(Size);
-    auto reduced_array_MPI = std::vector<int, boost::simd::allocator<int>>(Size);
+template<size_t Size, typename Reducer, typename Accumulator>
+void reduce_and_accumulate(boost::mpi::communicator const &world, Reducer reducer, Accumulator accumulator) {
+    auto local = std::vector<int, boost::simd::allocator<int>>(Size);
+    auto reduced = std::vector<int, boost::simd::allocator<int>>(Size);
 
-    // the element of array is generated randomly
-    std::generate(local_array.begin(), local_array.end(), [] { return rand(); });
+    srand(1);  // useful for testing
+    std::generate(local.begin(), local.end(), [] { return rand(); });
 
-    // MPI_Reduce and then print the usetime, the result will be put in res2[]
+    world.barrier();
+    auto use_time = time_function([&] {
+        reducer(world, &local.front(), static_cast<int>(local.size()), &reduced.front(), std::plus<>(), 0);
+    });
+    if (world.rank() > 0) { return; }
+
+    std::cout << Size << " int, use_time: " << use_time.count() << "us [" << Reducer::name << "]\n";
     {
-        world.barrier();
-        auto use_time2 = time_function([&] {
-            boost::mpi::reduce(world, &local_array.front(), static_cast<int>(local_array.size()),
-                               &reduced_array_MPI.front(), std::plus<>(), 0);
-        });
-        if (world.rank() == 0) {
-            std::cout << Size << " int, use_time: " << use_time2.count() << "us [MPI_Reduce]\n";
-        }
+        int sum = 0;
+        auto use_time2 = time_function([&] { sum = accumulator(&reduced.front(), &reduced.back(), 0); });
+        std::cout << "sum: " << sum << ", use_time: " << use_time2.count() << "us [" << Accumulator::name << "]\n";
     }
-
-    // YOUR_Reduce and then print the usetime, the result should be put in res[]
-    {
-        world.barrier();
-        auto use_time = time_function([&] {
-            boost::mpi::reduce(world, &local_array.front(), static_cast<int>(local_array.size()),
-                               &reduced_array.front(), std::plus<>(), 0);
-        });
-        if (world.rank() == 0) {
-            std::cout << Size << " int, use_time: " << use_time.count() << "us [YOUR_Reduce]\n";
-        }
-    }
-
-    // check the result of MPI_Reduce and YOUR_Reduce
-    if (world.rank() == 0) {
-        auto correctness = boost::simd::equal(&reduced_array.front(), &reduced_array.back(), &reduced_array_MPI.front());
-        if (!correctness) {
-            printf("WRONG !!!\n");
-        }
-        else {
-            printf("CORRECT !\n");
-        }
-
-        {
-            uint64_t sum = 0;
-            auto use_time = time_function([&] {
-                sum = static_cast<uint64_t>(boost::simd::reduce(&reduced_array.front(), &reduced_array.back(), 0));
-            });
-            std::cout << "sum: " << sum << ", use_time: " << use_time.count() << "us [single thread]\n";
-        }
-
-        {
-            uint64_t sum = 0;
-            auto use_time = time_function([&] {
-                sum = static_cast<uint64_t>(boost::simd::reduce(&reduced_array.front(), &reduced_array.back(), 0));
-            });
-            std::cout << "sum: " << sum << ", use_time: " << use_time.count() << "us [multiple threads]\n";
-        }
-
-        std::cout << std::endl;
-    }
+    std::cout << std::endl;
 }
 
 int main(int argc, char *argv[]) {
     boost::mpi::environment env{argc, argv};
     boost::mpi::communicator world;
 
-    srand(1);  // useful for testing
+    reduce_and_accumulate<4>(world, MPI_reduce(), STD_accumulator());
+    reduce_and_accumulate<4>(world, MPI_reduce(), SIMD_accumulator());
 
-    reduce_and_accumulate<4>(world);
-    reduce_and_accumulate<64>(world);
-    reduce_and_accumulate<1024>(world);
-    reduce_and_accumulate<16384>(world);
-    reduce_and_accumulate<262144>(world);
-    reduce_and_accumulate<4194304>(world);
+    reduce_and_accumulate<64>(world, MPI_reduce(), STD_accumulator());
+    reduce_and_accumulate<1024>(world, MPI_reduce(), STD_accumulator());
+    reduce_and_accumulate<16384>(world, MPI_reduce(), STD_accumulator());
+    reduce_and_accumulate<262144>(world, MPI_reduce(), STD_accumulator());
+
+    reduce_and_accumulate<4194304>(world, MPI_reduce(), STD_accumulator());
+    reduce_and_accumulate<4194304>(world, MPI_reduce(), SIMD_accumulator());
     return 0;
 }

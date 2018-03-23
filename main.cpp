8d9a4f5764ade313dedbfdfc0e0f8fb4b6d01ecf
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-03-23 12:32:36 +0800

    Improved reducing on vector

--- main.cpp
+++ main.cpp
@@ -86,12 +86,12 @@ struct smarter_reduce {
             }
         }
         if (comm.rank() != root) {
-            if (n <= 64) {
+//            if (n <= 64) {
                 MPI_Send((recv_count > 0) ? out_values : in_values, n, boost::mpi::get_mpi_datatype<T>(*out_values), comm.rank() - (j == 0 ? 1 : j + j), 0, comm);
-            }
-            else {
-                MPI_Rsend((recv_count > 0) ? out_values : in_values, n, boost::mpi::get_mpi_datatype<T>(*out_values), comm.rank() - (j == 0 ? 1 : j + j), 0, comm);
-            }
+//            }
+//            else {
+//                //MPI_Rsend((recv_count > 0) ? out_values : in_values, n, boost::mpi::get_mpi_datatype<T>(*out_values), comm.rank() - (j == 0 ? 1 : j + j), 0, comm);
+//            }
         }
     }
 };

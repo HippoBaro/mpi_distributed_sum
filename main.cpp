7ca2a5397022205a646d3d3adb9200289e0bea1c
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-03-23 16:30:09 +0800

    Improved reducing on vector

--- main.cpp
+++ main.cpp
@@ -37,42 +37,46 @@ struct dumb_accumulator {
     }
 };
 
+template <size_t Size>
 struct MPI_reduce {
     static constexpr auto name = "MPI_reduce";
 
     template<typename T, typename Op>
-    void operator()(const boost::mpi::communicator &comm, const T *in_values, int n, T *out_values, Op op, int root) {
-        boost::mpi::reduce(comm, in_values, n, out_values, op, root);
+    void operator()(const boost::mpi::communicator &comm, const T *in_values, T *out_values, Op op, int root) {
+        boost::mpi::reduce(comm, in_values, Size, out_values, op, root);
     }
 };
 
 std::vector<int, boost::simd::allocator<int>> responses(4194304);
 
+template <size_t Size>
 struct dumb_reduce {
     static constexpr auto name = "dumb_reduce";
 
     template<typename T, typename Op>
-    void operator()(const boost::mpi::communicator &comm, T *__restrict__ in_values, int n, T * __restrict__ out_values, Op op, int root) {
+    void operator()(const boost::mpi::communicator &comm, T *__restrict__ in_values, T * __restrict__ out_values, Op op, int root) {
         if (unlikely(comm.rank() == root)) {
             for (int j = 0; j < comm.size() - 1; ++j) {
-                comm.recv(boost::mpi::any_source, boost::mpi::any_tag, responses.data(), n);
-                std::transform(in_values, in_values + n, responses.data(), in_values, op);
+                comm.recv(boost::mpi::any_source, boost::mpi::any_tag, responses.data(), Size);
+                std::transform(in_values, in_values + Size, responses.data(), in_values, op);
             }
-            std::memcpy(out_values, in_values, n);
+            std::memcpy(out_values, in_values, Size);
         }
-        else { comm.send(root, 0, in_values, n); }
+        else { comm.send(root, 0, in_values, Size); }
     }
 };
 
 constexpr std::array<int, 9> log2_a{ {-1, 0, 1, -1, 2, -1, -1, -1, 3} };
 
-struct smarter_reduce {
+template<size_t Size>
+struct smarter_reduce : public dumb_reduce<Size> {
     static constexpr auto name = "smarter_reduce";
+
     template<typename T, typename Op>
-    void operator()(const boost::mpi::communicator &comm, T * __restrict__ in_values, int n, T * __restrict__ out_values, Op op, int root) {
+    void impl(const boost::mpi::communicator &comm, T * __restrict__ in_values, T * __restrict__ out_values, Op op, int root)  {
         if (likely (!(comm.rank() % 2))) {
-            comm.recv(comm.rank() + 1, boost::mpi::any_tag, responses.data(), n);
-            std::transform(in_values, in_values + n, responses.data(), out_values, op);
+            comm.recv(comm.rank() + 1, boost::mpi::any_tag, responses.data(), Size);
+            std::transform(in_values, in_values + Size, responses.data(), out_values, op);
         }
 
         int recv_count;
@@ -82,13 +86,22 @@ struct smarter_reduce {
         int j = 0;
         if (likely (recv_count > 0)) {
             for (j=1; !(comm.rank() % 2) && j < recv_count; ++j) {
-                comm.recv(comm.rank() + (j == 0 ? 1 : j + j), boost::mpi::any_tag, responses.data(), n);
-                std::transform(out_values, out_values + n, responses.data(), out_values, op);
+                comm.recv(comm.rank() + (j == 0 ? 1 : j + j), boost::mpi::any_tag, responses.data(), Size);
+                std::transform(out_values, out_values + Size, responses.data(), out_values, op);
             }
         }
         if (likely (comm.rank() != root)) {
-            MPI_Send((recv_count > 0) ? out_values : in_values, n, boost::mpi::get_mpi_datatype<T>(*out_values), comm.rank() - (j == 0 ? 1 : j + j), 0, comm);
+            MPI_Send((recv_count > 0) ? out_values : in_values, Size, boost::mpi::get_mpi_datatype<T>(*out_values),
+                     comm.rank() - (j == 0 ? 1 : j + j), 0, comm);
         }
+    }
+
+    template<typename T, typename Op>
+    void operator()(const boost::mpi::communicator &comm, T * __restrict__ in_values, T * __restrict__ out_values, Op op, int root)  {
+        if (Size > 1024)
+            impl(comm, in_values, out_values, op, root);
+        else
+            dumb_reduce<Size>::operator()(comm, in_values, out_values, op, root);
     }
 };
 
@@ -109,7 +122,7 @@ auto reduce_and_accumulate(boost::mpi::communicator const &comm, Reducer reducer
     std::generate(local.begin(), local.begin() + Size, [] { return rand(); });
 
     comm.barrier();
-    auto reduce_time = time_function([&] { reducer(comm, &local.front(), Size, &reduced.front(), std::plus<>(), 0); });
+    auto reduce_time = time_function([&] { reducer(comm, &local.front(), &reduced.front(), std::plus<>(), 0); });
     if (likely (comm.rank() > 0)) { return std::make_pair(0, 0); }
     auto accumulate_time = time_function([&] {
         volatile __attribute__((unused)) auto t = accumulator(reduced.data(), reduced.data() + Size, 0);
@@ -132,11 +145,11 @@ auto make_stat(Function &&function) {
     return std::make_pair(min_reduce, min_accumulate);
 }
 
-template<size_t Size, typename Reducer, typename Accumulator>
+template<size_t Size, template<size_t> class Reducer, typename Accumulator>
 void benchmark(boost::mpi::communicator const &comm) {
-    auto min = make_stat<100>([&comm] { return reduce_and_accumulate<Size>(comm, Reducer(), Accumulator()); });
+    auto min = make_stat<100>([&comm] { return reduce_and_accumulate<Size>(comm, Reducer<Size>(), Accumulator()); });
     if (comm.rank() > 0) { return; }
-    std::cout << "[Size: " << Size << "] " << Reducer::name << ": " << min.first << "us, "
+    std::cout << "[Size: " << Size << "] " << Reducer<Size>::name << ": " << min.first << "us, "
               << Accumulator::name << ": " << min.second << "us" << std::endl;
 }
 
